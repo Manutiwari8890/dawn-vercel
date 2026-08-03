518a1ac3728e229679b97b71dac235c49e2417b4
Manu <tiwarimanu871@gmail.com>
2026-08-03 19:33:09 +0530

    update search

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -3,7 +3,7 @@ import ProductClient from "./ProductClient";
 
 async function getProduct(slug) {
     const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  const res = await fetch(`https://new.dawnscientific.com/public/api/products/${slug}`, {
+  const res = await fetch(`${BASE_URL}products/${slug}`, {
                     method: 'GET',
                     headers: {
                         "Content-Type": "application/json",
