04fc729f21d7108a72494388567e4026624889b5
Manu <tiwarimanu871@gmail.com>
2026-08-07 17:52:30 +0530

    update blog

--- src/app/blog/BlogClient.jsx
+++ src/app/blog/BlogClient.jsx
@@ -61,7 +61,7 @@ export default function BlogClient() {
                                                         <h3>{single?.title}</h3>
                                                     </div>
                                                     <div className="btn-area mt-2">
-                                                        <Link href={`/blog/${single?.slug}`} className="btn btn-primary w-100">
+                                                        <Link href={`/blog/detail/${single?.slug}`} className="btn btn-primary w-100">
                                                             Read More 
                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" width="20" height="20"><path d="M566.6 342.6C579.1 330.1 579.1 309.8 566.6 297.3L406.6 137.3C394.1 124.8 373.8 124.8 361.3 137.3C348.8 149.8 348.8 170.1 361.3 182.6L466.7 288L96 288C78.3 288 64 302.3 64 320C64 337.7 78.3 352 96 352L466.7 352L361.3 457.4C348.8 469.9 348.8 490.2 361.3 502.7C373.8 515.2 394.1 515.2 406.6 502.7L566.6 342.7z" fill="currentColor" /></svg>
                                                         </Link>
